1fe574aabc92e6b51d009d02a65d61b1cdffc521
Anthony Chiara <achiara12@gmail.com>
2019-07-29 23:26:58 -0400

    first commit movie list

---
+++ Movie.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieList
+{
+    public class Movie
+    {
+        #region Fields
+
+        private string title;
+        private string category;
+
+        #endregion
+
+        #region Properties
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+            set
+            {
+                category = value;
+            }
+        }
+        #endregion
+
+        #region
+
+        public Movie()
+        {
+
+        }
+
+        public Movie(string _title, string _category)
+        {
+            title = _title;
+            category = _category;
+        }
+
+        #endregion
+
+
+
+        public static int TryParseInput(List<string>connectedCategory)
+        {
+            try
+            {
+                string input = Console.ReadLine();//
+                int user = int.Parse(input);
+                user--;
+                return user;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Not a number.");
+                return TryParseInput(connectedCategory);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("number way too big.");
+                return TryParseInput(connectedCategory);
+            }
+        }
+
+
+        public static void DisplayTasks()
+        {
+
+            Console.WriteLine("\nWhat category are you interested in?\n");
+            Console.WriteLine("Type a number:\n");
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MovieList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Movie> classics = new List<Movie>()
+            {
+                new Movie("The Departed", "drama"),
+                new Movie("Frozen", "animated"),
+                new Movie("Star Wars", "scifi"),
+                new Movie("Hereditary", "horror"),
+                new Movie("BladeRunner", "scifi"),
+                new Movie("Barton Fink", "drama"),
+                new Movie("Up", "animated"),
+                new Movie("The Shining", "horror"),
+                new Movie("Skyfall","drama"),
+                new Movie("The Dark Knight","drama")
+            };
+
+            List<string> filmCategory = new List<string> { "drama", "scifi", "horror", "animated" };
+
+            Console.WriteLine("Welcome to the Movie List Applicaton.");
+            Console.WriteLine("\nThere ten movies in this list.\n");
+            bool repeat = true;
+            while (repeat)
+            {
+                Movie.DisplayTasks();
+                for (int i = 0; i < filmCategory.Count; i++) //applies count increment of 
+                    //List of genres to Display method
+                {
+                    Console.WriteLine($"{i + 1}. {filmCategory[i]}");//1.drama 2.scifi etc
+                }
+                int input = Movie.TryParseInput(filmCategory);//when you make actual input selection
+                //from keyboard
+                try
+                {
+                    string title = filmCategory[input];
+                    foreach (Movie thing in classics)
+                    {
+                        if (thing.Category == title) //sauing if the thing you type is equal to string
+                                                     //thats equal to the list of filmCategory and its index
+                                                     //it will give the title
+                        {
+                            Console.WriteLine($"{thing.Title}");
+                        }
+                    }
+                }
+                catch(ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Number isnt in the range.");
+                    repeat = true;//not bringing me back up top.
+                }
+                
+
+                try
+                {
+                    
+                    string furSure = "";
+                    while(furSure!= "y" && furSure !="n")
+                    {
+                        Console.WriteLine("Would you like to view other categories? y/n");
+                        furSure = Console.ReadLine().ToLower();
+                    }
+
+                    if (furSure == "y")
+                    {
+                        repeat = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Goodbye!");
+                        repeat = false;
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"{e.GetType()}: {e.Message}");
+                }
+                
+            }
+            
+        }
+    }
+}
